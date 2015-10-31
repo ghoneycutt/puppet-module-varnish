9fdc0302f63a14bed5816ecad790ee4683b3c57e
Garrett Honeycutt <code@garretthoneycutt.com>
2015-10-31 19:30:25 -0400

    Fix spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 describe 'varnish' do
+  let(:facts) do
+    { :osfamily                  => 'RedHat',
+      :operatingsystemmajrelease => '6',
+    }
+  end
 
   context 'with defaults for all parameters' do
     it { should contain_class('varnish') }
+
+    it { should compile.with_all_deps }
   end
 end
