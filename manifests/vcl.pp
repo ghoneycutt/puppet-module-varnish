a2adbb9a3f62b155110905053daa65900600b7da
Phil Friderici <phil.friderici@i-tee.de>
2016-11-19 12:28:04 +0000

    Ensure inherited param when define is called direct

--- manifests/vcl.pp
+++ manifests/vcl.pp
@@ -5,19 +5,24 @@
 define varnish::vcl (
   $ensure   = 'present',
   $content  = undef,
-  $vcl_path = $::varnish::vcl_path,
+  $vcl_path = undef,
   $owner    = 'root',
   $group    = 'root',
   $mode     = '0644',
 ) {
 
+  include ::varnish
+
+  $vcl_path_real = $vcl_path ? {
+    undef   => $::varnish::vcl_path,
+    default => $vcl_path,
+  }
+
   if $ensure == 'present' and $content == undef {
     fail('varnish::vcl::ensure is present but varnish::vcl::content is undef. Please specify content.')
   }
 
-  include ::varnish
-
-  file { "${vcl_path}/${name}":
+  file { "${vcl_path_real}/${name}":
     ensure  => $ensure,
     content => $content,
     owner   => $owner,
